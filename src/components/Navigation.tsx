6fa310c819e18396840fb52aa86e96fd5b746ab7
aspara4242 <satorrrrru@gmail.com>
2025-12-19 12:15:47 +0900

    修正: ナビゲーションコンポーネントを追加し、フッターにニュースリンクを追加

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,26 @@
+// components/Navigation.tsx
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Navigation() {
+  return (
+    <div className="hidden fixed w-dvw md:flex justify-between items-center pt-6">
+
+      <Link href="/" className='inline-block ml-6'>
+        <Image
+          className="h-9 w-auto"
+          src="/logo_white.svg"
+          alt="団体ロゴ"
+          width={30}
+          height={30}
+        />
+      </Link>
+
+      <div className='text-lg text-right mr-6'>
+        <Link href="/about">About Us</Link> / <Link href="/works">Works</Link> / <Link href="/members">Members</Link> / <Link href="/news">News</Link>
+      </div>
+
+    </div>
+  )
+}

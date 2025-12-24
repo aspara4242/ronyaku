ab4da4e5a3ad5cc5ca451f760b78815434b1fad0
aspara4242 <satorrrrru@gmail.com>
2025-12-24 17:43:50 +0900

    修正: newsリストのカテゴリ表示のスタイルを調整し、works詳細ページにタイトルと戻るリンクを追加。グローバルスタイルを整理し、worksコンテンツのスタイルを改善。

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -2,9 +2,11 @@ import { getWorksDetail } from "@libs/works";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 import Contents from "@/components/Contents";
+import Link from "next/link";
+import Title from "@/components/Title";
 
 export default async function StaticDetailPage({
-  params
+  params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
@@ -16,19 +18,27 @@ export default async function StaticDetailPage({
       <Navigation />
 
       <Contents>
-        <div>
-          <p>{new Date(works.date).getFullYear()}</p>
-          <p>{works.subtitle && (works.subtitle)}</p>
+        <div className="mb-6 border-b text-center pb-4">
+        <h2 className="mb-1 text-center text-lg font-bold md:text-xl">
+          {works.title}
+        </h2>
+        <p className="text-center text-sm md:text-base">
+          {new Date(works.date).getFullYear()}
+          {works.subtitle && " / " + works.subtitle}
+        </p>
         </div>
-        <h2>{works.title}</h2>
         <div
           dangerouslySetInnerHTML={{
             __html: `${works.body}`,
           }}
+          className="works-content prose prose-sm mb-16 md:prose-base"
         />
+        <p className="w-full text-center text-sm underline md:text-base">
+          <Link href="/works">一覧に戻る</Link>
+        </p>
       </Contents>
 
       <Footer />
     </div>
-  )
+  );
 }

cfd3d828ead60380937d6a6bb099be7e173ab742
aspara4242 <satorrrrru@gmail.com>
2026-01-07 16:41:42 +0900

    テキストのline-heightを調整。X-Robots-Tagの設定を追加。

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  async headers() {
+    const headers = [];
+    if (process.env.VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      });
+    }
+    return headers;
+  },
 };
 
 export default nextConfig;
--- src/components/TextBlockSm.tsx
+++ src/components/TextBlockSm.tsx
@@ -12,9 +12,9 @@ const TextBlockSm = ({ title, children, level = "h3" }: TextBlockProps) => {
 
   return (
     <div className="mb-4">
-      <Tag className="mb-1 text-sm font-bold md:text-base">{title}</Tag>
+      <Tag className="mb-2 text-sm font-bold md:text-base">{title}</Tag>
 
-      <div className="text-justify text-sm leading-7 md:text-base md:leading-7">
+      <div className="text-justify text-sm leading-[1.75] md:text-base md:leading-[1.75]">
         {children}
       </div>
     </div>

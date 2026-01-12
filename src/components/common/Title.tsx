9795b2a5e5425e7348b7f3c12719a20c917e0441
aspara4242 <satorrrrru@gmail.com>
2026-01-12 22:39:28 +0900

    コンポーネントの構成を変更その2

---
+++ src/components/common/Title.tsx
@@ -0,0 +1,18 @@
+// components/Navigation.tsx
+import React from "react";
+
+interface TitleProps {
+  title: string;
+  ja_title: string;
+}
+
+const Title = ({ title, ja_title }: TitleProps) => {
+  return (
+    <div>
+      <h1 className="mb-2 text-xl font-bold">{title}</h1>
+      <p className="mb-12 text-sm font-bold">{ja_title}</p>
+    </div>
+  );
+};
+
+export default Title;
